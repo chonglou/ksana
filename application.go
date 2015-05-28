bc16ae81930910cff956096c8e78584e060b87aa
flamen <jitang.zheng@gmail.com>
2015-05-28 10:19:49 -0700

    nginx config file

--- application.go
+++ application.go
@@ -25,7 +25,7 @@ type Application interface {
 }
 
 func New() (Application, error) {
-	actions := []string{"server", "migrate", "rollback", "routes", "db", "redis"}
+	actions := []string{"server", "migrate", "rollback", "routes", "db", "redis", "nginx"}
 	cfg := flag.String("c", "config.json", "configuration file name")
 	act := flag.String("r", "server", "running: "+strings.Join(actions, " | "))
 	flag.Parse()
@@ -147,10 +147,98 @@ func (app *application) Start() error {
 	case "redis":
 		cmd, args := app.redis.Shell()
 		err = Shell(cmd, args...)
+	case "nginx":
+		app.nginx()
 	default:
 	}
 
 	return err
+}
+
+func (app *application) nginx() {
+	hn, _ := os.Hostname()
+	wd, _ := os.Getwd()
+
+	var buf bytes.Buffer
+	fmt.Fprintf(
+		&buf,
+		`
+upstream ksana.conf {
+	server http://localhost:%d fail_timeout=0;
+}
+`,
+		app.config.Web.Port)
+	fmt.Fprintf(
+		&buf,
+		`
+server {
+	listen 443;
+	ssl  on;
+	ssl_certificate  ssl/ksana.crt;
+	ssl_certificate_key  ssl/ksana.key;
+	ssl_session_timeout  5m;
+	ssl_protocols  SSLv2 SSLv3 TLSv1;
+	ssl_ciphers  RC4:HIGH:!aNULL:!MD5;
+	ssl_prefer_server_ciphers  on;
+
+	client_max_body_size 4G;
+	keepalive_timeout 10;
+
+	server_name %s;
+
+	root %s/public;
+	try_files $uri/index.html $uri @ksana.conf;
+
+	location @ksana.conf {
+		proxy_set_header X-Forwarded-Proto https;
+		proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+		proxy_set_header Host $http_host;
+		proxy_set_header  X-Real-IP $remote_addr;
+		proxy_redirect off;
+		proxy_pass http://ksana.conf;
+		# limit_req zone=one;
+		access_log log/ksana.access.log;
+		error_log log/ksana.error.log;
+	}
+
+	location ^~ /assets/ {
+		gzip_static on;
+		expires max;
+		add_header Cache-Control public;
+	}
+
+	location = /50x.html {
+		root html;
+	}
+
+	location = /404.html {
+		root html;
+	}
+
+	location @503 {
+		error_page 405 = /system/maintenance.html;
+		if (-f $document_root/system/maintenance.html) {
+			rewrite ^(.*)$ /system/maintenance.html break;
+		}
+		rewrite ^(.*)$ /503.html break;
+	}
+
+	if ($request_method !~ ^(GET|HEAD|PUT|PATCH|POST|DELETE|OPTIONS)$ ){
+		return 405;
+	}
+
+	if (-f $document_root/system/maintenance.html) {
+		return 503;
+	}
+
+	location ~ \.(php|jsp|asp)$ {
+		return 405;
+	}
+
+}
+		`, hn, wd)
+	fmt.Fprintf(&buf, "\n")
+	buf.WriteTo(os.Stdout)
 }
 
 func (app *application) shell(cmd string, args ...string) error {
